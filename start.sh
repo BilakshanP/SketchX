9f86f921675ee2003c0b2b695933b99859e0da1a
BilakshanP <pbilakshan@gmail.com>
2023-12-13 19:56:04 +0530

    vercel support

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
+if [ ! -d ".venv" ]; then
+    python3 -m venv .venv
+    source .venv/bin/activate
+    echo "To exit the virtual environment type 'deactivate' in the shell."
+fi
+
 pip3 install -U -r requirements.txt
 
 python3 -m Main

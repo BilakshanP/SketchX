eefe064a50dbcb2d30e19e4fb2dd3ae36fbb3ccf
BilakshanP <pbilakshan@gmail.com>
2023-12-16 22:21:42 +0530

    start.sh imporoved

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 if [ ! -d ".venv" ]; then
     python3 -m venv .venv
-    source .venv/bin/activate
-    echo "To exit the virtual environment type 'deactivate' in the shell."
 fi
+
+
+source .venv/bin/activate
+# echo "To exit the virtual environment type 'deactivate' in the shell."
 
 pip3 install -U -r requirements.txt
 
 python3 -m Main
+
+deactivate
